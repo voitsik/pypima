a9383c9dc5a5d2c5b193ff6a5db4e596953a88a9
Petr Voytsik <voitsik@asc.rssi.ru>
2013-12-25 04:57:56 +0400

    Rework FITS-file downloading code
    Add '_update_cnt' method

--- pima.py
+++ pima.py
@@ -61,11 +61,33 @@ def get_antab(exper_name, band, dist_dir):
     return antab_path
 
 
-class Pima:
-    data_base = '/home/voitsik/data/VLBI/RA/ASC_results/'
-    ftp_base = 'ftp://quasars:JcSzi5_k@archive.asc.rssi.ru'
+def get_uvfits_url(exper_name, band):
+    url = None
+    url_base = 'ftp://quasars:JcSzi5_k@archive.asc.rssi.ru'
 
-    def __init__(self, exper, band, uv_fits=None, url=None, orbit=None):
+    conn = psycopg2.connect(database='ra_results', user='guest', host='odin',
+                            password='tufJoorit3')
+    conn.autocommit = True
+    cur = conn.cursor()
+
+    cur.execute('SELECT path FROM fits_files WHERE exper_name = %s \
+                 AND band = %s ORDER BY corr_date DESC;',
+               (exper_name, band.upper()))
+    path = cur.fetchone()
+    cur.close()
+    conn.close()
+
+    if path:
+        path = path[0]
+        url = url_base + path
+
+    return url
+
+
+class Pima:
+    data_dir_base = '/home/voitsik/data/VLBI/RA/ASC_results/'
+
+    def __init__(self, exper, band, uv_fits=None, orbit=None):
         # First, set common variables
         self.exper = exper.lower()
         self.band = band.lower()
@@ -104,42 +126,30 @@ class Pima:
             os.mkdir(antab_dir)
         self.antab = antab_dir + '/' + self.exper + self.band + '.antab'
 
+        # PIMA control file path
         self.cnt_file_name = self.work_dir + '/' + \
             self.exper + '_' + self.band + '_' + 'pima.cnt'
 
         # Prepare data paths
-        self.url = url
         self.uv_fits = uv_fits
         self.orbit = orbit
 
-        self.data_dir = self.data_base + self.exper
+        self.data_dir = self.data_dir_base + self.exper
+
+        # Get fits-file url from DB if don't specified explicitly
         if not self.uv_fits:
-            self._set_fits_name()
+            self.uv_fits = get_uvfits_url(self.exper, self.band)
+            if self.uv_fits is None:
+                raise Exception('Could not get UV-FITS name from DB for \
+                    experiment {}'.format(self.exper))
+            local_file_name = '{}/{}'.format(self.data_dir,
+                              os.path.basename(self.uv_fits))
+            if os.path.isfile(local_file_name):
+                self.uv_fits = local_file_name
+
         if not self.orbit:
             self._get_orbit()
         self._mk_cnt()
-
-    def _set_fits_name(self):
-        if not self.url:
-            conn = psycopg2.connect(database='ra_results', user='voitsik')
-            conn.autocommit = True
-            cur = conn.cursor()
-
-            cur.execute('SELECT path FROM fits_files WHERE exper_name = %s \
-                         AND band = %s ORDER BY corr_date DESC;',
-                       (self.exper, self.band.upper()))
-            path = cur.fetchone()
-            cur.close()
-            conn.close()
-
-            if not path:
-                raise Exception('Could not get UV-FITS name from DB for the \
-                experiment {}, band {}'.format(self.exper, self.band))
-            else:
-                path = path[0]
-            self.url = self.ftp_base + path
-
-        self.uv_fits = self.data_dir + '/' + os.path.basename(self.url)
 
     def _mk_cnt(self):
         cnt_templ_name = self.pima_dir + '/share/pima/EXPER_' + self.band + \
@@ -151,17 +161,21 @@ class Pima:
             if 'CDATE' in line:
                 line = line.replace('CDATE', str(datetime.datetime.now()))
             elif line.startswith('SESS_CODE:'):
-                line = 'SESS_CODE:      ' + self.exper + '_' + self.band + '\n'
+                line = '{:<20}{}_{}\n'.format('SESS_CODE:',  self.exper,
+                       self.band)
             elif line.startswith('EXPER_DIR:'):
                 line = line.replace('SCRDIR', self.pima_scr)
             elif line.startswith('UV_FITS:'):
-                line = 'UV_FITS:            ' + self.uv_fits + '\n'
-            elif line.startswith('BANDPASS_FILE:') or \
-                    line.startswith('BANDPASS_MASK_FILE:') or \
+                line = '{:<20}{}\n'.format('UV_FITS:', self.uv_fits)
+            elif line.startswith('BANDPASS_MASK_FILE:') or \
                     line.startswith('FRINGE_FILE:') or \
                     line.startswith('FRIRES_FILE:'):
                 line = line.replace('EXP_DIR', self.exp_dir)
                 line = line.replace('EXPERN', self.exper)
+            elif line.startswith('BANDPASS_FILE:'):
+                bps_file_name = \
+                    '{}/{}_{}.bps'.format(self.work_dir, self.exper, self.band)
+                line = '{:<20}{}\n'.format('BANDPASS_FILE:', bps_file_name)
             elif line.startswith('EPHEMERIDES_FILE:'):
                 line = 'EPHEMERIDES_FILE:   ' + self.orbit + '\n'
             cnt_file.write(line)
@@ -170,13 +184,17 @@ class Pima:
         cnt_file.close()
 
     def _download_fits(self):
-        if not os.path.isdir(self.data_dir):
-            os.mkdir(self.data_dir)
+        local_file_name = '{}/{}'.format(self.data_dir,
+                          os.path.basename(self.uv_fits))
 
         if not os.path.isfile(self.uv_fits):
+            if not os.path.isdir(self.data_dir):
+                os.mkdir(self.data_dir)
             print('Info: Start downloading file {}...'.format(self.uv_fits))
-            urllib.request.urlretrieve(self.url, filename=self.uv_fits)
+            new_file_name, _ = urllib.request.urlretrieve(self.uv_fits,
+                               filename=local_file_name)
             print('Info: Done')
+            self.uv_fits = new_file_name
         else:
             print('Info: file {} already exists'.format(self.uv_fits))
 
@@ -208,6 +226,26 @@ class Pima:
         else:
             print('Info: file {} already exists'.format(self.orbit))
 
+    def _update_cnt(self, opts):
+        """Update pima cnt-file according 'opts' dictionary"""
+        if opts is None:
+            return
+
+        old_cnt = open(self.cnt_file_name, 'r')
+        new_cnt = open(self.cnt_file_name + '.new', 'w')
+
+        for line in old_cnt:
+            key = line.split()[0].strip()
+            if key in opts.keys():
+                line = '{:<20}{}\n'.format(key, opts[key])
+            elif line.startswith('# Last update on'):
+                line = '# Last update on  {}\n'.format(str(datetime.datetime.now()))
+            new_cnt.write(line)
+
+        old_cnt.close()
+        new_cnt.close()
+        os.rename(self.cnt_file_name + '.new', self.cnt_file_name)
+
     def _exec(self, operation, options=None, log_name=None):
         """Execute PIMA binary"""
         cmd_line = [self.pima_exec, self.cnt_file_name, operation]
@@ -232,10 +270,11 @@ class Pima:
     def load(self):
         if not os.path.exists(self.uv_fits):
             self._download_fits()
+            self._update_cnt({'UV_FITS:': self.uv_fits})
 
         if not os.path.isfile(self.antab):
             self.antab = get_antab(self.exper, self.band, self.work_dir +
-                '/antab')
+                         '/antab')
             if self.antab is None:
                 raise Exception('Could not get antab-file')
             print('DEBUG: antab -> {}'.format(self.antab))
@@ -258,6 +297,8 @@ class Pima:
         if os.path.isfile(frr_file):
             os.remove(frr_file)
 
+        self._update_cnt({'STA_REF:': 'BADARY'})
+
         opts = ['FRINGE_FILE:',             fri_file,
                 'FRIRES_FILE:',             frr_file,
                 'BANDPASS_USE:',            'NO',
@@ -268,8 +309,7 @@ class Pima:
                 'FRIB.SECONDARY_SNR_MIN:',   '0.0',
                 'FRIB.SECONDARY_MAX_TRIES:', '0',
                 'FRIB.FINE_SEARCH:', 'PAR',
-                'MKDB.FRINGE_ALGORITHM:',   'DRF',
-                'STA_REF:', 'BADARY']
+                'MKDB.FRINGE_ALGORITHM:',   'DRF']
         ret = self._exec('frib', opts, log_name)
         if ret:
             raise Exception('coarse failed with code {}'.format(ret))
