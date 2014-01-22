4f142563d5db70a91f924e34db9eb215df78ead2
Petr Voytsik <voitsik@asc.rssi.ru>
2014-01-22 21:11:50 +0400

    test.py: cmd line arguments and exception catching

--- test.py
+++ test.py
@@ -7,7 +7,17 @@ Created on Fri Dec 13 17:50:20 2013
 """
 
 import raexperiment
+import sys
 
-p = raexperiment.RaExperiment('raes03dz', 'c', 'BADARY')
-p.load()
-p.fringe_fitting()
+if len(sys.argv) != 3:
+    print('Usage: {} <exper> <band>'.format(sys.argv[0]))
+    sys.exit(1)
+
+try:
+    p = raexperiment.RaExperiment(sys.argv[1], sys.argv[2])
+    p.load()
+    #print(p.pima.sta_list())
+    p.fringe_fitting(True)
+except Exception as ex:
+    print('Exception: {}'.format(ex))
+    sys.exit(1)
