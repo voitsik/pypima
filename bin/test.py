69efcf09ca9d5ab60bf434da149dd0d6467da315
Petr Voytsik <voitsik@asc.rssi.ru>
2014-05-26 16:33:49 +0400

    bin/test.py: Update

--- bin/test.py
+++ bin/test.py
@@ -9,36 +9,49 @@ Created on Fri Dec 13 17:50:20 2013
 from __future__ import print_function
 import sys
 import os.path
-path = os.path.normpath(os.path.join(os.path.dirname(sys.argv[0]), '..'))
-sys.path.insert(0, path)
+PATH = os.path.normpath(os.path.join(os.path.dirname(sys.argv[0]), '..'))
+sys.path.insert(0, PATH)
 import pypima
 from pypima.raexperiment import RaExperiment
 
-if len(sys.argv) != 3:
-    print('Usage: {} <exper> <band>'.format(sys.argv[0]), file=sys.stderr)
-    sys.exit(2)
 
-try:
-    p = RaExperiment(sys.argv[1], sys.argv[2])
-    p.load()
+def main():
+    """Main"""
+    if len(sys.argv) != 3:
+        print('Usage: {} <exper> <band>'.format(sys.argv[0]), file=sys.stderr)
+        sys.exit(2)
 
-    for polar in ['RR', 'LL']:
+    exper = sys.argv[1].lower()
+    band = sys.argv[2].lower()
+
+    try:
+        p = RaExperiment(exper, band)
+        p.load()
+
+        polar = 'LL'
+        if band == 'l':
+            polar = 'RR'
+
         p.pima.set_polar(polar)
         p.fringe_fitting(True, True)
-#        p.fringes2db()
+    #        p.fringes2db()
         p.split()
 
-#    p.delete_uvfits()
-except pypima.pima.Error as err:
-    print('PIMA Error: ', err)
-    p.db.set_error_msg(str(err))
-    sys.exit(1)
-except pypima.raexperiment.Error as err:
-    print('RaExperiment Error: ', err)
-    sys.exit(1)
-except KeyboardInterrupt:
-    print('KeyboardInterrupt', file=sys.stderr)
-    exit(1)
-except:
-    print("Unexpected error: ", sys.exc_info()[0])
-    raise
+    #    p.delete_uvfits()
+    except pypima.pima.Error as err:
+        print('PIMA Error: ', err)
+        p.db.set_error_msg(str(err))
+        sys.exit(1)
+    except pypima.raexperiment.Error as err:
+        print('RaExperiment Error: ', err)
+        sys.exit(1)
+    except KeyboardInterrupt:
+        print('KeyboardInterrupt', file=sys.stderr)
+        exit(1)
+    except:
+        print("Unexpected error: ", sys.exc_info()[0])
+        raise
+
+
+if __name__ == "__main__":
+    main()
