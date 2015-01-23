7f000d5e8e4e72be87f418c6de1ba844c0b503e3
Petr Voytsik <voitsik@asc.rssi.ru>
2015-01-23 12:40:08 +0300

    bin/test.py

--- bin/test.py
+++ bin/test.py
@@ -36,12 +36,12 @@ def main():
         p.pima.set_polar(polar)
         p.fringe_fitting(True, True)
 
-        spec_out_dir = os.path.join(os.getenv('HOME'), 'public_ftp', 'ra_data',
-                                    'pima_autospec')
-        if not os.path.isdir(spec_out_dir):
-            os.mkdir(spec_out_dir)
-
-        p.generate_autospectra(spec_out_dir)
+#        spec_out_dir = os.path.join(os.getenv('HOME'), 'public_ftp', 'ra_data',
+#                                    'pima_autospec')
+#        if not os.path.isdir(spec_out_dir):
+#            os.mkdir(spec_out_dir)
+#
+#        p.generate_autospectra(spec_out_dir)
 
         p.split()
 
