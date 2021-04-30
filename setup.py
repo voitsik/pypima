5445434efe676beace84376d2e5fe1758ff1053d
Petr Voytsik <voitsik@asc.rssi.ru>
2021-04-30 23:05:03 +0300

    Update package setup files

--- setup.py
+++ setup.py
@@ -1,22 +1,6 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
-from distutils.core import setup
+import setuptools
 
-import pypima
-
-setup(
-    name="pypima",
-    version=pypima.__version__,
-    description="Python interface to PIMA",
-    author="Petr Voytsik",
-    author_email="voitsik@asc.rssi.ru",
-    url="https://github.com/voitsik/pypima",
-    packages=["pypima"],
-    scripts=[
-        "bin/pima_coher.py",
-        "bin/pima_plot_fringe3d.py",
-        "bin/pypima_batch.py",
-        "bin/pypima_single.py",
-    ],
-)
+if __name__ == "__main__":
+    setuptools.setup()
